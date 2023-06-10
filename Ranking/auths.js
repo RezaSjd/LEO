d435bebb694b67dd35bb298337e160f01cf76406
RezaSjd <126521763+RezaSjd@users.noreply.github.com>
2023-06-11 02:33:09 +0330

    Add files via upload

---
+++ Ranking/auths.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+	"discordToken":"MTExNzIwNjI1MTM0OTIzMzczNQ.GpC6Cj.TsuFOMeEdsnfR-uj1TEsZSKjYze9KT4gHiaICg",
+	"prefix":"$"
+}
---
+++ cmd/about.js
@@ -0,0 +1,29 @@
+﻿const Discord = require('discord.js')
+
+exports.run = (bot, msg, params) => {
+
+  var invite = new Discord.RichEmbed()
+
+          .addField("__**" + "سازنده : " + "**__", ":warning:  AghayeSajjadian#9321 :warning: ", true)
+          .addField("__**" + "وزژن  : " + "**__", "0.4.2 :robot: ", false)
+		  .addField("__**" + "وضیت ریات : " + "**__", "آنلاین :white_check_mark: ", true)
+          .addField("__**" + "چنل پشتیبانی : " + "**__", "https://discord.gg/abG3Hd7aQt :envelope: ", true)
+        
+          .setColor("0x#a2f634")
+
+  msg.channel.send({embed: invite});
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "about",
+  description: "اطلاعات ربات",
+  usage: "about"
+};
